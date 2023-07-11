0b847f20d72ff15d1034498f0c4e0ec18358dba0
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-07-11 22:06:00 +1000

    homework done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+price = 10000
+quantity = 1
+discount = 0.85
+def test_calculate_price():
+    assert price * quantity == 10000
+
+def test_apply_discount():
+    assert price * discount == 8500
