06b91cee5d55e3f203dddc4083db11b28200d79c
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-07-13 23:43:58 +1000

    fix test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,25 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
+from src.item import Item
 
-price = 10000
-quantity = 1
+
 discount = 0.85
-def test_calculate_price():
-    assert price * quantity == 10000
 
-def test_apply_discount():
-    assert price * discount == 8500
+
+@pytest.fixture
+def item() -> Item:
+    return Item("Смартфон", 1000, 2)
+
+
+def test_item_initialized(item) -> None:
+    assert item.name == "Смартфон"
+    assert item.price == 1000
+    assert item.quantity == 2
+
+
+def test_calculate_total_price(item):
+    assert item.price * item.quantity == 2000
+
+
+def test_apply_discount(item):
+    assert item.price * discount == 850
