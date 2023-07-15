421482e83e1f6976adda71615e575c811ee2c482
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-07-16 02:36:35 +1000

    fix test

--- src/item.py
+++ src/item.py
@@ -1,10 +1,12 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int):
         """
@@ -14,21 +16,47 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Срезает строку, если больше 10 символов"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv"""
+        cls.all.clear()
+        items = []
+        with open('../src/items.csv', newline='', encoding='windows-1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                _ = cls(row['name'], row['price'], row['quantity'])
+                items.append(_)
+
+        return items
+
+    @staticmethod
+    def string_to_number(number: str) -> float:
+        """Статический метод, возвращающий число из числа-строки"""
+        return round(int(float(number)))
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
         """
 
         return self.price * self.quantity
-
 
     def apply_discount(self) -> float:
         """
@@ -36,3 +64,6 @@ class Item:
         """
         self.price *= self.pay_rate
         return self.price
+
+
+Item.instantiate_from_csv()
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,7 +2,6 @@
 import pytest
 from src.item import Item
 
-
 discount = 0.85
 
 
@@ -23,3 +22,20 @@ def test_calculate_total_price(item):
 
 def test_apply_discount(item):
     assert item.price * discount == 850
+
+
+def test_instantiate_from_csv(item):
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+    item2 = Item.all[1]
+    assert item2.name == 'Ноутбук'
+    item2 = Item.all[1]
+    assert item2.price == '1000'
+    item3 = Item.all[2]
+    assert item3.quantity == '5'
+
+
+def test_string_to_number(item):
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number("6.7") == 6
+    assert Item.string_to_number("3.3") == 3
